be9fe75cbd1d21738cd7e9eed26f2dffdb920d85
ahagelthorn1 <114444563+ahagelthorn1@users.noreply.github.com>
2025-07-23 12:08:55 +0100

    Delete games/logic_pipes/src/scenes directory

--- games/logic_pipes/src/scenes/Gate.js
+++
@@ -1,9 +0,0 @@
-export class Gate extends Phaser.GameObjects.Sprite
-{
-    constructor(scene,x,y,texture,scale,type) {
-        super(scene,x,y,texture);
-        scene.add.existing(this);
-        this.setScale(scale);
-        this.type = type;
-    }
-}
